aa347ba11f799903b1a650789142f6c50831fa21
mikeezvz <mike.zogheib@outlook.com>
2024-11-28 13:24:12 +0100

    GET + POST endpoints finished
    GET for all Tasks and POST for creating a new task finished

--- backend/routes/tasks.js
+++ backend/routes/tasks.js
@@ -1,21 +1,43 @@
 const express = require('express');
-const mongoose = require ('mongoose')
 const router = express.Router();
-router.use(express.json());
+const mongoose = require('mongoose')
 
-router.get('/all', async(request, response) => {
-    await mongoose.connect('mongodb://127.0.0.1/my_database');
-    const Schema = mongoose.Schema;
-const ObjectId = Schema.ObjectId;
+const taskSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    description: { type: String },
+    completed: { type: Boolean, default: false },
+  });
+  
+  const Task = mongoose.model('tasks', taskSchema);
 
-const BlogPost = new Schema({
-  author: ObjectId,
-  title: String,
-  body: String,
-  date: Date
+router.get('/all', async (request, response) => {
+    try {
+        const tasks = await Task.find();    
+        response.json(tasks);
+        console.log('Data received')
+    } catch (error) {
+        response.status(500).send('Tasks can not be loaded from DB')
+        console.error('Data can not be received:', error)
+    }
+
 });
+
+router.post('/new', async (request, response) => {
+    try{
+    const doc = new Task({
+        title: request.body.title,
+        description: request.body.description,
+        completed: request.body.completed
+      });
+    const savedTask = await doc.save();
+    response.status(201).json(savedTask)
+    console.log('Document saved')
+    }
+    catch (error) {
+        response.status(500).send('Document can not be saved')
+        console.error('Error saving task:', error)
+    }
+
 });
 
-router.post('/new', (request, response) => {
-
-});
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -4,17 +4,30 @@ const mongoose = require('mongoose');
 const app = express();
 const port = 5000;
 const session = require('express-session');
-const bodyParser = require('body-parser');
 const router = express.Router();
-router.use(express.json());
 
-
-app.use(bodyParser.json());
+app.use(express.json());
+app.use(cors());
 app.use(session({
     secret: 'your_secret_key',
     resave: false,
     saveUninitialized: true
 }));
+
+const connectDB = async () => {
+    try {
+      await mongoose.connect('mongodb+srv://mike_zvz:mn0tavRJAiW1bsEr@tasks.vnvee.mongodb.net/task-tracker', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      });
+      console.log('Connection to MongoDB successful');
+    } catch (error) {
+      console.error('Verbindung zu MongoDB fehlgeschlagen:', error);
+      process.exit(1);
+    }
+  };
+
+  connectDB();
 
 const tasksRoute = require('./routes/tasks');
 app.use('/tasks', tasksRoute);

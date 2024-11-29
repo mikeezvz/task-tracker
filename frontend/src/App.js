0523471cc2b3892e7447dd01420945fb6413ef58
mikeezvz <mike.zogheib@outlook.com>
2024-11-29 20:50:55 +0100

    tasks render view added
    Tasks are now being rendered in the React frontend and the frontend sucessfully communicates with the backend. Additionally react-bootstrap was added.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
+import { Container, Row, Col, Card } from 'react-bootstrap';
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [tasks, setTasks] = useState([])
+  useEffect(() => {
+    const fetchTasks = async () => {
+      try {
+        const response = await fetch('http://localhost:5000/tasks/all');
+        if (response.ok) {
+          const data = await response.json();
+          setTasks(data);
+        } else {
+          setTasks(null);
+        }
+      } catch (error) {
+        console.error('Can not fetch API', error);
+      }
+    };
+  
+    fetchTasks();
+  }, []); 
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Row>
+        <Col>
+          <div id="header">
+            Task-Tracker
+          </div>
+        </Col>
+        <Row>
+        {tasks.map((task, index) => (
+          <Col key={index} sm={12} md={6} lg={4} className="mb-4">
+            <Card>
+              <Card.Body>
+                <Card.Title>{task.title}</Card.Title>
+                <Card.Text>{task.description}</Card.Text>
+                <Card.Text>
+                  <strong>Status:</strong> {task.completed ? "Completed" : "Not Completed"}
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+        
+      </Row>
+    </Container>
   );
 }
 
